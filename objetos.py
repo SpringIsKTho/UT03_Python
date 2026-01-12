9593e585f268a72b8d15002e7af0110848e44c13
SpringIsKTho <sergiodela2006@gmail.com>
2026-01-12 12:11:57 +0100

    objetos cambiado

--- objetos.py
+++ objetos.py
@@ -11,39 +11,3 @@ alumno = [
     {"nombre":"Patricia", "edad":19, "nota":6.25, "Juego Favorito":"Animal Crossing", "deuda_publica":"0 (miente)"},
     {"nombre":"Enrique", "edad":42, "nota":8.8, "Juego Favorito":"Mario Kart", "deuda_publica":"0 (miente)"}
 ]
-
-edadActual = 19
-media = 0
-cuenta = 0
-maxNota = ["nombre", 0]
-minNota = ["nombre", 10]
-lista = []
-
-for clave in alumno:
-    lista.append(clave["edad"])
-
-moda = max(lista, key=lista.count)
-
-for clave in alumno:
-    print(f"Nombre: {clave["nombre"]}, Edad: {clave["edad"]}, Nota: {clave["nota"]}, Juego Favorito: {clave["Juego Favorito"]} Deuda pública: {clave["deuda_publica"]}")
-    cuenta+=1
-    media+=clave["edad"]
-    if maxNota[1]<clave["nota"]:
-        maxNota = [clave["nombre"], clave["nota"]]
-    if minNota[1]>clave["nota"]:
-        minNota = [clave["nombre"], clave["nota"]]
-
-print("\n")
-print(f"Media de edades: {int(media/cuenta)} \n")
-print(f"Nota más grande: {maxNota} - Edad más pequeña: {minNota} \n")
-print(f"Edad más repetida: {moda} \n")
-
-filtro = int(input("Dame una edad para filtrar: "))
-found = False
-for clave in alumno:
-    if clave["edad"] == filtro:
-        found = True
-        print(f"Nombre: {clave["nombre"]}, Edad: {clave["edad"]}, Nota: {clave["nota"]}, Juego Favorito: {clave["Juego Favorito"]} Deuda pública: {clave["deuda_publica"]}")
-
-if found == False:
-    print("Edad no encontrada.")
